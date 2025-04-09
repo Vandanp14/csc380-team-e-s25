6cfa4e4eaa3d4080ceed704f9ff6a49f2f69717e
Emma Halsey <160976732+emhalsey@users.noreply.github.com>
2025-04-09 19:04:06 -0400

    adding querying

---
+++ backend/src/main/resources/querying.py
@@ -0,0 +1,21 @@
+import requests
+import mysql.connector
+from datetime import datetime
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host="pi.cs.oswego.edu",
+    user="CSC380_25S_TeamE",
+    password="Bu$Tr@ck3r1000_?",
+    database="CSC380_25S_TeamE"
+)
+cursor = conn.cursor()
+
+cursor.execute(
+    "SELECT Stops.stpnm,Routes.des,prdtm,prdctdn,tmpstmp,dly,rtdir FROM ETA INNER JOIN Routes ON ETA.rt=Routes.rt INNER JOIN Stops ON ETA.stpid=Stops.stpid LIMIT 1"
+)
+
+myresult = cursor.fetchall()
+
+for x in myresult:
+  print(x)
