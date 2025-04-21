6874c2d234f559147306dcd9b7b59162b2296c98
Vandanp14 <vpatel4@oswego.edu>
2025-04-21 13:26:36 -0400

    before dynamically update code snap.

---
+++ src/api.js
@@ -0,0 +1,2 @@
+// src/api.js
+export const BACKEND_URL = "http://129.3.20.26:5001"; 
---
+++ src/services/apiService.js
@@ -0,0 +1,31 @@
+// src/services/apiService.js
+import { BACKEND_URL } from '../api';
+
+export async function getRoutes() {
+  const response = await fetch(`${BACKEND_URL}/routes`);
+  return await response.json();
+}
+
+export async function getPrediction(route, stop) {
+  try {
+    const response = await fetch(`${BACKEND_URL}/prediction?route=${route}&stop=${stop}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch prediction');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching prediction:', error);
+    return [];
+  }
+}
+
+export async function getSchedule(route) {
+  const response = await fetch(`${BACKEND_URL}/schedule?route=${route}`);
+  return await response.json();
+}
+
+export async function getGraphAnalysis(route, direction) {
+  const response = await fetch(`${BACKEND_URL}/graphanalysis?route=${route}&direction=${direction}`);
+  return await response.json();
+}
